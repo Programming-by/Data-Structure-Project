851d90869e388c7e2df1c965b61585611031ff5a
AnasNaamneh <naamnih.anas@gmail.com>
2023-04-27 03:43:07 -0700

    Inherit MyQueueArr in MyStackArr

---
+++ clsMyStackArr.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include "clsMyQueueArr.h"
+
+template <class T>
+class clsMyStackArr : public clsMyQueueArr <T>
+{
+protected:
+
+
+public:
+
+	void push(T Item)
+	{
+		
+		clsMyQueueArr <T>::_MyList.InsertAtBeginning(Item);
+	}
+
+	T Top() {
+		return clsMyQueueArr <T>::front();
+	}
+
+	T Bottom() {
+		return clsMyQueueArr <T>::back();
+	}
+
+
+};
+
